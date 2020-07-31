8434baba8bcfaaf256ebb9011ceb7cada47a2af7
Mariusz Rokicki <mrokicki@onet.pl>
2020-07-31 20:28:35 +0200

    Fixed minor bug

--- lists/views.py
+++ lists/views.py
@@ -21,4 +21,3 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
-
